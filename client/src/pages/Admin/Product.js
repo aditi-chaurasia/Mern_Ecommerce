5513004a46e2a4dfefe94394991d2e328721a72e
Aditi Chaurasia <103958459+aditi-chaurasia@users.noreply.github.com>
2024-07-11 23:35:19 +0530

    Product.js

--- client/src/pages/Admin/Product.js
+++ client/src/pages/Admin/Product.js
@@ -1,15 +1,15 @@
 import React, { useEffect, useState } from 'react';
-import { Layout } from 'antd';
 import AdminMenu from '../../components/Layout/AdminMenu';
 import axios from 'axios';
 import toast from 'react-hot-toast';
 import { useAuth } from '../../context/auth';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-import {Link} from 'react-router-dom'
+import {Link} from 'react-router-dom';
+import Layout from '../../components/Layout/Layout'
 
 
-const { Content } = Layout;
+
 
 const Product = () => {
     const [products, setProducts] = useState([]);
@@ -21,7 +21,7 @@ const Product = () => {
             const { data } = await axios.get(`${process.env.REACT_APP_API}/api/product/get-product`,
                 {
                     headers: {
-                        Auth: auth.token, // Include the token in the request headers
+                        Authorization: `Bearer ${auth.token}`                    
                     },
                 }
             );
@@ -33,16 +33,16 @@ const Product = () => {
 
     useEffect(() => {
         getAllProducts();
-    }, []); // Empty dependency array ensures useEffect runs only once
+    }, []); 
 
     return (
-        <Layout style={{ minHeight: '100vh' }}>
-            <AdminMenu />
-            <Layout className="site-layout">
-                <Content style={{ margin: '16px' }}>
+        <Layout title={"Dashboard - Get Product"}>
+            <div className='col-md-3'>
+           <AdminMenu/>
+        </div>
                     <div className='row'>
                         {products?.map((p) => (
-                            <>
+                          
                              <div className='col-md-3 mb-4 mr-2 mx-auto'>
                             <Link  key={p._id} to = {`/dashboard/admin/product/${p.slug}`}>
                            
@@ -51,17 +51,16 @@ const Product = () => {
                                     src={`${process.env.REACT_APP_API}/api/product/product-photo/${p._id}`} />
                                     <Card.Body>
                                         <Card.Title>{p.name}</Card.Title>
-                                        <Card.Text>{p.description}</Card.Text>
-                                        <Button variant="primary">Go somewhere</Button>
+                                        <Card.Text>{p.description.substring(0,30)}....</Card.Text>
+                                        <Button variant="primary">VIEW PRODUCT</Button>
                                     </Card.Body>
                                 </Card>
                            
-                            </Link> </div>
-                            </>
+                            </Link> 
+                            </div>
+                         
                         ))}
                     </div>
-                </Content>
-            </Layout>
         </Layout>
     );
 };
